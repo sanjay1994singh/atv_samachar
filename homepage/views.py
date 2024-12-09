22d1cfaea8040a097ec7bc7954320f11ae8ac204
sanjay <sanjaypython22@gmail.com>
2024-12-09 09:38:35 +0530

    p

---
+++ homepage/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from news.models import News
+
+
+# Create your views here.
+def homepage(request):
+    news = News.objects.all().order_by('-id')
+    context = {
+        'news': news,
+    }
+    return render(request, 'index.html', context)
