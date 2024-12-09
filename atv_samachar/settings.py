c63dc6588dbb3ad8b8dbf9bbdd7b99ec45926fd9
sanjay <sanjaypython22@gmail.com>
2024-12-09 09:54:06 +0530

    p

--- atv_samachar/settings.py
+++ atv_samachar/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'atv_samachar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'mbdb/db.sqlite3',
     }
 }
 
